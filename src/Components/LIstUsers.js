3ae451630a90d644da4f3e59fafe0a246129fd76
Meriem Ouni <meriemouni@MacBook-Air-de-Meriem.local>
2021-10-26 14:05:54 +0100

    first commit

---
+++ src/Components/LIstUsers.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import UserCard from './UserCard'
+import './UserList.css'
+
+const LIstUsers = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const url ='https://jsonplaceholder.typicode.com/users'
+        axios.get(url)
+        .then(response => response.data)
+        .then(resData => setUsers(resData))
+    }, [])
+
+    return (
+        <div >
+            <h2>Welcome, this is the List of Users</h2>
+            <div className='user-container'>
+                {users.map( user => <UserCard user={user} key={user.id}/> )}
+            </div>
+        </div>
+    )
+}
+
+export default LIstUsers
